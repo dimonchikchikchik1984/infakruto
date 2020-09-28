4a042c6acffb4e04f9e7d67e16a64002cca3da0d
ovsy2 <asdfdsdsdsdxd@mail.ru>
2020-09-28 19:11:28 +0300

    Вот первое упражнение

--- testing.py
+++ testing.py
@@ -1,4 +1,21 @@
-with open("something.txt", "w") as out:
-    for i in range(100):
-        out.write("А я запишу все эти строки в влюбом случае\n")
-    raise Exception
+from sys import argv
+from sys import exit
+def fib(n):
+    f=[0]*n
+    f[0]=f[1]=1
+    for i in range (2,n):
+        f[i]=f[i-1]+f[i-2]
+    return f[n-1]
+if __name__=="__main__":
+        if len(argv)==3:
+            if argv[1]=='-n':
+                print(str(fib(int(argv[2]))))
+            else:
+                print('Invalid argument')
+                exit(1)
+        else:
+            if len(argv)==2:
+                print(str(fib(int(argv[1]))))
+            else:
+                print('Incorrect number of arguments')
+                exit(1)
