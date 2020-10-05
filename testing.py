92020eb8430ea8f386dc509d240d9a9179c2e062
ovsy2 <asdfdsdsdsdxd@mail.ru>
2020-10-05 18:35:57 +0300

    Упражнение по ООП №1

--- testing.py
+++ testing.py
@@ -1,21 +1,15 @@
-from sys import argv
-from sys import exit
-def fib(n):
-    f=[0]*n
-    f[0]=f[1]=1
-    for i in range (2,n):
-        f[i]=f[i-1]+f[i-2]
-    return f[n-1]
-if __name__=="__main__":
-        if len(argv)==3:
-            if argv[1]=='-n':
-                print(str(fib(int(argv[2]))))
-            else:
-                print('Invalid argument')
-                exit(1)
-        else:
-            if len(argv)==2:
-                print(str(fib(int(argv[1]))))
-            else:
-                print('Incorrect number of arguments')
-                exit(1)
+class Shape:
+    def breadth(self):
+        self.b=float(input())
+    def height(self):
+        self.h=float(input())
+
+
+class Rectangle(Shape):
+    def area(self):
+        print(self.b*self.h)
+
+
+class Triangle(Shape):
+    def area(self):
+        print(0.5*self.b*self.h)
