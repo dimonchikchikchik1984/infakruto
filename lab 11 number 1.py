5d3aeb2b92167dee6747da10af50782e38c3deae
ovsy2 <asdfdsdsdsdxd@mail.ru>
2020-11-29 23:13:36 +0300

    Объяснен вывод

---
+++ lab 11 number 1.py
@@ -0,0 +1,37 @@
+import asyncio
+
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({i})...")
+        await asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f}")
+
+async def main():
+    await asyncio.gather(
+        factorial("A", 2),
+        factorial("B", 3),
+        factorial("C", 4),
+    )
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
+'''
+Output:
+Task A: Compute factorial(2)...
+Task B: Compute factorial(2)...
+Task C: Compute factorial(2)...
+Task A: factorial(2) = 2
+Task B: Compute factorial(3)...
+Task C: Compute factorial(3)...
+Task B: factorial(3) = 6
+Task C: Compute factorial(4)...
+Task C: factorial(4) = 24
+'''
+'''
+Процесс А захватывает программу и засыпает, тем временем процесс В начинает работу и тоже засыпает, процесс С захватывает управление
+Тем временем процесс А возобновляется, выходит из цикла и выводит ответ. Процесс В возобновляется и переходит на следующую итерацию
+Далее аналогично
+'''
